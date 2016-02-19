144386aca92f4b7729abb02a5e9c307abbddb781
xavier <xschildwachter@hotmail.com>
2016-02-19 00:19:48 -0800

    xfer

--- src/main/java/org/sagebionetworks/remotefilepreviewgenerator/ImageMagickPreviewGenerator.java
+++ src/main/java/org/sagebionetworks/remotefilepreviewgenerator/ImageMagickPreviewGenerator.java
@@ -3,6 +3,16 @@ package org.sagebionetworks.remotefilepreviewgenerator;
 import java.io.File;
 
 public class ImageMagickPreviewGenerator implements ExternalPreviewGenerator {
+	
+	private ImageMagickProvider provider;
+	
+	public void setImageMagickProvider(ImageMagickProvider provider) {
+		this.provider = provider;
+	}
+	
+	public ImageMagickProvider getImageMagickProvider() {
+		return this.provider;
+	}
 
 	@Override
 	public File generatePreview(File input) {
